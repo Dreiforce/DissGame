acaeaf72e3c0a31f0e72364273f3f1bd7b541e56
Jakob Geringer <jakob.geringer@sprad.io>
2023-03-25 00:20:25 +0100

    add character animation

--- scenes/character.gd
+++ scenes/character.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 100.0
 const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -9,6 +9,7 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(_delta):
 	# Add the gravity.
+	change_animation()
 	if not is_on_floor():
 		velocity.y += gravity * _delta
 
@@ -23,5 +24,23 @@ func _physics_process(_delta):
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	move_and_slide()	
 
-	move_and_slide()
+
+
+func change_animation():
+	if velocity.x > 0:
+		$AnimatedSprite2D.flip_h = false
+	if velocity.x < 0:
+		$AnimatedSprite2D.flip_h = true
+	
+	
+	if velocity.y != 0: # negative Y is up
+		$AnimatedSprite2D.play("jump")
+	else:
+		if velocity.x != 0:
+			$AnimatedSprite2D.play("walk")
+		else:
+			$AnimatedSprite2D.play("idle")
+
