a18e7b85e379ef16879abee470496b35ea0f0d88
Jakob Geringer <jakob.geringer@sprad.io>
2023-03-24 23:34:39 +0100

    add jump behaviour

--- scenes/character.gd
+++ scenes/character.gd
@@ -1,6 +1,5 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
@@ -19,10 +18,13 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+# jump will happen on the next frame
+	if Input.is_action_just_pressed("jump"):
+		if is_on_floor():
+			velocity.y = JUMP_VELOCITY # negative Y is up in Godot
 	move_and_slide()
